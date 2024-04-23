72c6be7c01304e9e09801d3ec63a16c4e40152cd
Leonel Sicaro <leonelsicaro@gmail.com>
2024-04-23 18:43:57 +0200

    server on with dotenv

---
+++ BACK/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+const App = express()
+
+
+const PORT = process.env.PORT || 3000
+
+App.listen(3000,()=>{
+    console.log(`server on http://localhost:${PORT}`)
+})
+
+App.get("/",(req,res)=>{
+    res.send("holiwis")
+})
